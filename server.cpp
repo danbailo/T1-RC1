59ba0c2eef692bd5704e2b8f4e379b5f3858dd7c
Daniel Bailo <danbailoufms@gmail.com>
2019-09-23 12:09:26 -0400

    ja entendi o erro q ta dando, preciso arrumar a mensagem qnd ele envia algum caractere diferente

--- server.cpp
+++ server.cpp
@@ -6,95 +6,96 @@
 
 using namespace std;
 
+//Função para pegar a data do sistema.
 string curr_date(){
     time_t     now = time(0);
     struct tm  tstruct;
     char       buf[80];
     tstruct = *localtime(&now);
-    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
-    // for more information about date/time format
     strftime(buf, sizeof(buf), "%d/%m/%Y", &tstruct);
-    // strftime(buf, sizeof(buf), "%Y/%m/%d.%X", &tstruct);
-
     return buf;
 }
 
+//Função para pegar a hora do sistema.
 string curr_time(){
     time_t     now = time(0);
     struct tm  tstruct;
     char       buf[80];
     tstruct = *localtime(&now);
-    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
-    // for more information about date/time format
     strftime(buf, sizeof(buf), "%X", &tstruct);
-    // strftime(buf, sizeof(buf), "%Y/%m/%d.%X", &tstruct);
     return buf;
 }
 
-//Server side
-int main(int argc, char *argv[])
-{
-    //for the server, we only need to specify a port number
-    if(argc != 2)
-    {
+//Lado do Servidor
+int main(int argc, char *argv[]){
+
+    //Nessa parte iremos forçar o usuário entrar com a porta do servidor.
+    if(argc != 2){
         cerr << "Informe uma porta para executar" << endl;
         exit(0);
     }
-    //grab the port number
+
+    //Atribui a Porta infomada pelo usuário a esta variável.
     int port = atoi(argv[1]);
-    //buffer to send and receive messages with
+    
+    //Cria um buffer para enviar e receber dados(mensagens);
     char msg[1500];
      
-    //setup a socket and connection tools
+    //Configura o socket e as ferramentas de conexão.
     sockaddr_in servAddr;
     bzero((char*)&servAddr, sizeof(servAddr));
     servAddr.sin_family = AF_INET;
     servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servAddr.sin_port = htons(port);
  
-    //open stream oriented socket with internet address
-    //also keep track of the socket descriptor
+    //Ssocket orientado a fluxo aberto com endereço da Internet e também acompanha o descritor de socket
     int serverSd = socket(AF_INET, SOCK_STREAM, 0);
     if(serverSd < 0){
         cerr << "Erro estabelecendo socket do servidor!" << endl;
         exit(0);
     }
-    //bind the socket to its local address
-    int bindStatus = bind(serverSd, (struct sockaddr*) &servAddr, 
-        sizeof(servAddr));
+
+    //Vincula o socket com o endereço local.
+    int bindStatus = bind(serverSd, (struct sockaddr*) &servAddr, sizeof(servAddr));
     if(bindStatus < 0){
         cerr << "Erro ao criar socket com porta! tente outra." << endl;
         exit(0);
     }
     cout << "Esperando o cliente se conectar..." << endl;
-    //listen for up to 5 requests at a time
+
+    //Aceita até 5 solicitações por vez.
     listen(serverSd, 5);
-    //receive a request from client using accept
-    //we need a new address to connect with the client
+
+    //Recebe uma solicitação do cliente usando a função "accept".
+    //Precisamos de um novo endereço para conectar com o cliente.
     sockaddr_in newSockAddr;
     socklen_t newSockAddrSize = sizeof(newSockAddr);
-    //accept, create a new socket descriptor to 
-    //handle the new connection with client
+
+    // "accept", cria um novo descritor de soquete para manipular a nova conexão com o cliente.
     int newSd = accept(serverSd, (sockaddr *)&newSockAddr, &newSockAddrSize);
-    if(newSd < 0)
-    {
+    if(newSd < 0){
         cerr << "Erro aceitando requisicao do cliente!" << endl;
         exit(1);
     }
     cout << "Cliente conectado!" << endl;
 
-    //also keep track of the amount of data sent as well
+    //Acompanha a quantidade de dados enviados também.
     int bytesRead, bytesWritten = 0;
     
-    bool flag = true;
-    while(flag){
-        memset(&msg, 0, sizeof(msg));//clear the buffer
+    int state = 1;
+    while(state){
+        memset(&msg, 0, sizeof(msg));//Limpa o buffer.
         bytesRead += recv(newSd, (char*)&msg, sizeof(msg), 0);            
 
         string data;
-        if(atoi(msg) != 0){
-            //receive a message from the client (listen)
 
+        // cout << "msg: " << msg << endl;
+        // cout << "atoi msg: " << atoi(msg) << endl;
+
+        // Se ele enviar algo diferente das opções, vai retornar um 0 e não entra nesse if.
+        if(atoi(msg) != 0){
+            cout << "nao entrei\n";
+            // Recebe uma mensagem do cliente(escuta).
             cout << "\nCliente enviou: " << msg << endl;
             switch(atoi(msg)){
             case 1:
@@ -113,14 +114,22 @@ int main(int argc, char *argv[])
                 data = "Não compreendi a sua solicitação!";
                 break;
             }	
-            memset(&msg, 0, sizeof(msg)); //clear the buffer
+            memset(&msg, 0, sizeof(msg));//Limpa o buffer.
             strcpy(msg, data.c_str());    
-            //send the message to client
+            
+            //Envia a mensagem para o cliente.
             bytesWritten += send(newSd,(char*)&msg,strlen(msg),0);
         }
-        else flag = false;
+        else{
+            cout << "ENTREI NO ELSE\n";
+            memset(&msg, 0, sizeof(msg));//Limpa o buffer.
+            strcpy(msg, data.c_str());    
+            //Envia a mensagem para o cliente.
+            bytesWritten += send(newSd,(char*)&msg,strlen(msg),0);
+            state = 0; //Modifica o estado do automato para parar a execução.
+        }
     }
-    //we need to close the socket descriptors after we're all done
+    // Fecha os descritores do socket depois de tudo pronto.
     close(newSd);
     close(serverSd);
     return 0;   
